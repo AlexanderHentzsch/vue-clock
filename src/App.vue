a48462fb1ed23985165facd60b6cd255aa4c9fa3
BillAlex <billalex@hotmail.de>
2019-10-19 19:10:47 +0200

    v1.0

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app" class="container w3-display-container font-comfortaa" style="height: 100%">
+        &nbsp;
+        <div class="w3-display-middle" style="width: 210px">
+            <div>{{dayName}}, {{day}}.{{month}}.{{year}}</div>
+            <div>
+                <span style="font-size: 64px">{{ hour }}:{{ minute }}.</span>
+                <span style="font-size: 32px">{{ second }}</span>
+            </div>
+        </div>
+        <div style="clear:both"></div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    export default {
+        name: 'app',
+        data() {
+            return {
+                year: "2018",
+                month: "11",
+                day: "20",
+                dayName: "Sonntag",
+                hour: "",
+                minute: "",
+                second: "",
+                week: ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"]
+            }
+        },
+        mounted() {
+            this.updateTime();
+            setInterval(this.updateTime, 1000);
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+            this.setFullScreenModeByAngle();
+            if ("orientation" in screen) {
+                let o = screen.orientation;
+                o.addEventListener("change", this.setFullScreenModeByAngle, false);
+            }
+        },
+        methods: {
+            updateTime() {
+                let ct = new Date();
+                this.year = ct.getFullYear();
+                this.month = ct.getMonth() + 1;
+                this.day = ct.getDate();
+                this.dayName = this.week[ct.getDay()];
+                this.hour = ct.getHours();
+                this.minute = ct.getMinutes().toString().padStart(2, "0");
+                this.second = ct.getSeconds().toString().padStart(2, "0");
+            },
+            setFullScreenModeByAngle() {
+                let DOM = document.querySelector("#app");
+                let o = screen.orientation;
+                if (o.angle !== 0 && o.angle !== 180) {
+                    if (!document.fullscreenElement) {
+                        DOM.webkitRequestFullscreen();
+                        DOM.requestFullscreen();
+                    }
+                } else {
+                    if (document.fullscreenElement) {
+                        document.exitFullscreen();
+                    }
+                }
+            }
+        }
+    }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+    @import url('https://fonts.googleapis.com/css?family=Comfortaa&display=swap');
+
+    .font-comfortaa {
+        font-family: 'Comfortaa', cursive;
+    }
+
+    .container {
+        height: 100%;
+        background-color: #000;
+        color: #3E606F;
+    }
 </style>
---
+++ vue.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    publicPath: '/vue-clock/',
+    outputDir: 'docs'
+};
